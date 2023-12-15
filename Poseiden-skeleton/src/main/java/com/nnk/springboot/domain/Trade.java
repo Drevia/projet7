e5332719e66ba1ebbcbeb1871b7a65e3f943a304
Cyriac <drevia59@gmail.com>
2023-12-15 11:32:39 +0100

    fix model

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
@@ -1,6 +1,8 @@
 package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,10 +18,15 @@ import java.sql.Timestamp;
 public class Trade {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long tradeId;
+    private Long id;
 
+    @NotNull
+    @NotBlank
     private String account;
+    @NotNull
+    @NotBlank
     private String type;
+    @NotNull
     private Double buyQuantity;
     private Double sellQuantity;
     private Double buyPrice;
